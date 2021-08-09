027c5675f026698d8bdbe17d83ef70da98b24765
xeniya <xeniia.izotowa@gmail.com>
2021-08-10 00:27:47 +0300

    Added shouldFindMaxWhenMaxIsNotFirst

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldFindMaxWhenMaxIsNotFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
